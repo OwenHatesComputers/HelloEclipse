df11816514a9f5024f82cde2d80e30a0bacc1ea1
OwenHatesComputers <33526306+OwenHatesComputers@users.noreply.github.com>
2017-11-09 16:28:48 -0400

    Update HelloApp2.java

--- HelloProject2/src/HelloApp2.java
+++ HelloProject2/src/HelloApp2.java
@@ -3,7 +3,7 @@ public class HelloApp2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Owen 'Destroyer of Worlds!' \\m/ ");
+		System.out.println("Hello Owen 'Destroyer of Worlds!' Boyle \\m/ ");
 		
 	}
 
